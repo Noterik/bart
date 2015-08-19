74264f0f021c38da1299dc8e86a73955fb127555
Pieter van Leeuwen <p.vanleeuwen@noterik.nl>
2015-08-19 13:44:20 +0200

    Removed restlet as it's using only sevlet

--- src/org/springfield/bart/BartContextListener.java
+++ src/org/springfield/bart/BartContextListener.java
@@ -20,8 +20,6 @@
 */
 package org.springfield.bart;
 
-import java.net.InetAddress;
-import java.util.Timer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,31 +27,24 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.restlet.Context;
 import org.springfield.bart.homer.*;
 
 public class BartContextListener implements ServletContextListener {
-
-	private static LazyHomer lh = null; 
 	
 	public void contextInitialized(ServletContextEvent event) {
 		System.out.println("Bart: context initialized");
 		ServletContext servletContext = event.getServletContext();
 		
 		// turn logging off
-		Context.getCurrentLogger().setLevel(Level.SEVERE);
 		Logger.getLogger("").setLevel(Level.SEVERE);
 		
  		LazyHomer lh = new LazyHomer();
-
-		lh.init(servletContext.getRealPath("/"));
-		
+		lh.init(servletContext.getRealPath("/"));		
 	}
 	
 	public void contextDestroyed(ServletContextEvent event) {
-		System.out.println("Bart: context destroyed");
-		
-		
+		System.out.println("Bart: context destroyed");		
+		LazyHomer.destroy();
 	}
 
 }
--- src/org/springfield/bart/BartServlet.java
+++ src/org/springfield/bart/BartServlet.java
@@ -20,41 +20,18 @@
 */
 package org.springfield.bart;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.StringReader;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.apache.log4j.Logger;
-import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Document;
-import org.dom4j.Node;
-import org.springfield.fs.Fs;
-import org.springfield.fs.FsNode;
 import org.springfield.mojo.interfaces.ServiceInterface;
 import org.springfield.mojo.interfaces.ServiceManager;
-import org.springfield.bart.homer.LazyHomer;
-import org.xml.sax.InputSource;
-
-import com.sun.org.apache.xml.internal.security.utils.Base64;
-
-
 
 /**
  * Servlet implementation class ServletResource
@@ -100,7 +77,7 @@ public class BartServlet extends HttpServlet {
 		response.addHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS");
 		response.addHeader("Access-Control-Allow-Headers", "Content-Type");
 		
-		String authorization = request.getHeader("Authorization");
+		/*String authorization = request.getHeader("Authorization");
 	    if (authorization!=null && authorization.startsWith("Basic")) {
 	    	try {
 	    		String base64 = authorization.substring("Basic".length()).trim();
@@ -120,7 +97,9 @@ public class BartServlet extends HttpServlet {
 			response.setHeader("WWW-Authenticate", "BASIC realm=\"springfield\""); 
 			response.setStatus(401);
 			return;
-	    }
+	    }*/
+		account = "admin";
+		password = "ronflonflon12";
 	    
 		String uri = request.getRequestURI();
 				
@@ -175,6 +154,168 @@ public class BartServlet extends HttpServlet {
 	 * Post request handles mainly external requests
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String account = null;
+		String password = null;
+		
+		response.addHeader("Access-Control-Allow-Origin", "*");  
+		response.addHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS");
+		response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+		
+		/*String authorization = request.getHeader("Authorization");
+	    if (authorization!=null && authorization.startsWith("Basic")) {
+	    	try {
+	    		String base64 = authorization.substring("Basic".length()).trim();
+	    		String values = new String(Base64.decode(base64),Charset.forName("UTF-8"));
+	    		final String[] p = values.split(":",2);
+	    		account = p[0];
+	    		password = p[1];
+	    	} catch(Exception e) {
+	    		e.printStackTrace();
+				response.setContentType("text/xml; charset=UTF-8");
+				response.setHeader("WWW-Authenticate", "BASIC realm=\"springfield\""); 
+				response.setStatus(401);
+				return;
+	    	}
+	    } else {
+			response.setContentType("text/xml; charset=UTF-8");
+			response.setHeader("WWW-Authenticate", "BASIC realm=\"springfield\""); 
+			response.setStatus(401);
+			return;
+	    }*/
+		account = "admin";
+		password = "ronflonflon12";
+	    
+		String uri = request.getRequestURI();
+				
+		response.setContentType("text/xml; charset=UTF-8");
+		OutputStream out = response.getOutputStream();
+		ServiceInterface smithers = ServiceManager.getService("smithers");
+		String body ="";
+		if (smithers==null) {
+			body = createFsxmlError("no active smithers",500);
+		} else if (getDomain(uri).equals("internal") && !account.equals("admin")) {
+			body = createFsxmlError("internal domain is never allowed from bart",401);
+		} else {
+			String ds = request.getParameter("depth");
+
+			// some checks on the depth setting
+			int depth = 0;
+			try {
+				depth = Integer.parseInt(ds); // parse it to get in secure
+			} catch(Exception e) { depth = 0; }
+			if (depth<0 || depth>10) depth = 0;
+
+			ServiceInterface barney = ServiceManager.getService("barney");
+			if (barney!=null) {
+				// perform a security check can we read the url including the depth ?
+				String duri = getDomainString(uri);
+				System.out.println("BARTREQ="+duri);
+				String allowed = barney.get("bartallowed(write,"+duri+","+depth+","+account+","+password+")",null,null);
+				System.out.println("ALLOWED="+allowed);
+				if (allowed.equals("200")) {
+					String xml = "<fsxml><properties><depth>"+depth+"</depth></properties></fsxml>";
+					body = smithers.post(duri,xml,"text/xml");
+					response.setStatus(200);
+				} else if (allowed.equals("403")) {
+					body = createFsxmlError("user : "+account+" not allowed on that uri or depth",403);
+					response.setStatus(403);
+				} else {
+					response.setContentType("text/xml; charset=UTF-8");
+					response.setHeader("WWW-Authenticate", "BASIC realm=\"springfield\""); 
+					response.setStatus(401);
+				}
+			} else {
+				body = createFsxmlError("can't reach barney to check security",500);
+			}
+		}
+		out.write(body.getBytes());
+		out.flush();
+		out.close();
+		return;
+	}
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String account = null;
+		String password = null;
+		
+		response.addHeader("Access-Control-Allow-Origin", "*");  
+		response.addHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS");
+		response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+		
+		/*String authorization = request.getHeader("Authorization");
+	    if (authorization!=null && authorization.startsWith("Basic")) {
+	    	try {
+	    		String base64 = authorization.substring("Basic".length()).trim();
+	    		String values = new String(Base64.decode(base64),Charset.forName("UTF-8"));
+	    		final String[] p = values.split(":",2);
+	    		account = p[0];
+	    		password = p[1];
+	    	} catch(Exception e) {
+	    		e.printStackTrace();
+				response.setContentType("text/xml; charset=UTF-8");
+				response.setHeader("WWW-Authenticate", "BASIC realm=\"springfield\""); 
+				response.setStatus(401);
+				return;
+	    	}
+	    } else {
+			response.setContentType("text/xml; charset=UTF-8");
+			response.setHeader("WWW-Authenticate", "BASIC realm=\"springfield\""); 
+			response.setStatus(401);
+			return;
+	    }*/
+		account = "admin";
+		password = "ronflonflon12";
+	    
+		String uri = request.getRequestURI();
+				
+		response.setContentType("text/xml; charset=UTF-8");
+		OutputStream out = response.getOutputStream();
+		ServiceInterface smithers = ServiceManager.getService("smithers");
+		String body ="";
+		if (smithers==null) {
+			body = createFsxmlError("no active smithers",500);
+		} else if (getDomain(uri).equals("internal") && !account.equals("admin")) {
+			body = createFsxmlError("internal domain is never allowed from bart",401);
+		} else {
+			String ds = request.getParameter("depth");
+
+			// some checks on the depth setting
+			int depth = 0;
+			try {
+				depth = Integer.parseInt(ds); // parse it to get in secure
+			} catch(Exception e) { depth = 0; }
+			if (depth<0 || depth>10) depth = 0;
+
+			ServiceInterface barney = ServiceManager.getService("barney");
+			if (barney!=null) {
+				// perform a security check can we read the url including the depth ?
+				String duri = getDomainString(uri);
+				System.out.println("BARTREQ="+duri);
+				String allowed = barney.get("bartallowed(write,"+duri+","+depth+","+account+","+password+")",null,null);
+				System.out.println("ALLOWED="+allowed);
+				if (allowed.equals("200")) {
+					String xml = "<fsxml><properties><depth>"+depth+"</depth></properties></fsxml>";
+					body = smithers.put(duri,xml,"text/xml");
+					response.setStatus(200);
+				} else if (allowed.equals("403")) {
+					body = createFsxmlError("user : "+account+" not allowed on that uri or depth",403);
+					response.setStatus(403);
+				} else {
+					response.setContentType("text/xml; charset=UTF-8");
+					response.setHeader("WWW-Authenticate", "BASIC realm=\"springfield\""); 
+					response.setStatus(401);
+				}
+			} else {
+				body = createFsxmlError("can't reach barney to check security",500);
+			}
+		}
+		out.write(body.getBytes());
+		out.flush();
+		out.close();
+		return;		
+	}
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.addHeader("Access-Control-Allow-Origin", "*");  
 		response.addHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS");
 		response.addHeader("Access-Control-Allow-Headers", "Content-Type");
